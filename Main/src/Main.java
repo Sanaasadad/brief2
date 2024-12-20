7686a29b29385c0a7068aaf132eff988f4a98356
Sanaasadad <sanaasadad4@gmail.com>
2024-12-20 17:26:11 +0100

    version 3

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,11 +1,11 @@
 
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
    static ArrayList<Book> livres = new ArrayList<>();
-
-    static Scanner ser = new Scanner(System.in);
+   static Scanner ser = new Scanner(System.in);
 
     public static void Ajouter() {
 
@@ -22,11 +22,20 @@ public class Main {
        // livres.add(books);
         livres.add(new Book(titre, auteur, dispo, Isbn));
     }
-    public static void Afficher(){
-        for(int i=0;i<livres.size();i++) {
+    public static void Afficher() {
+        for (int i = 0; i < livres.size(); i++) {
             System.out.println(livres.get(i).toString());
         }
+    }
+    public static void Rechercher(){
+            System.out.println(" Entre l'isbn: ");
+            String Isbn = ser.nextLine();
+            for( Book livres : livres) {
+                if (livres.getIsbn().equals(Isbn)){
+                    System.out.println(livres);
 
+            }
+            }
     }
 
     public static void main(String[] args) {
@@ -36,7 +45,8 @@ public class Main {
             System.out.println("Menu: ");
             System.out.println(" 1-Ajouter");
             System.out.println(" 2-Afficher");
-            System.out.println(" 3-Modifier");
+            System.out.println(" 3-Rechercher");
+            System.out.println(" 4-Modifier");
             System.out.println(" 4-Supprimer");
             System.out.println(" 5-quitter");
             System.out.println(" choisissez une option");
@@ -50,9 +60,12 @@ public class Main {
                     Afficher();
                     break;
                 case 3:
+                    Rechercher();
+                    break;
+                case 4:
                     System.out.println("au revoir !!");
 
             }
-        } while (choix != 3);
+        } while (choix != 4);
     }
 }
